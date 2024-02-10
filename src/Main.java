17552d8e860ec34a88dd47f4c39487aacae5c03c
kalepriti-1 <kalepriti21@navgurukul.org>
2024-02-09 19:27:11 -0800

    first commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+        Set<String> treeSet = new HashSet<>();
+        treeSet.add("Z");
+        treeSet.add("e");
+        treeSet.add("B");
+        treeSet.add("A");
+        treeSet.add("C");
+        System.out.println(treeSet);
+    }
+}
